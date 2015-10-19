66033d99065f3a959847a6da6d6e6344a68760eb
Wayne <wayne@cantina.co>
2015-10-18 23:39:59 -0500

    Add confirm and prompt

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,19 @@
+var _ = require('lodash');
 var greet = require('./txt/01-greet.txt');
 var trick = require('./txt/02-trick.txt');
 
 window.alert(greet);
-window.alert(trick);
+_.delay(function() {
+    window.alert(trick);
+}, 1000);
+
+window.prompt('Are you feeling lucky', 'Do ya punk?');
+
+window.on('DOMWindowClose', function(e) {
+    if (window.confirm("Do you really want to leave?")) {
+      window.open("exit.html", "Thanks for Visiting!");
+  } else {
+      e.preventDefault();
+  }
+
+});
